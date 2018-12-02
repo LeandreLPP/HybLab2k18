02b2d73f126b05c6cd081ebb81c87e5486665e15
mdetante <maxence.detante@gmail.com>
2018-12-02 15:02:15 +0100

    Add squeletton of project

---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+const ws = new WebSlides({
+    autoslide: false,
+    changeOnClick: false,
+    loop: true,
+    minWheelDelta: 40,
+    navigateOnScroll: true,
+    scrollWait: 450,
+    slideOffset: 50,
+    showIndex: true
+  });
