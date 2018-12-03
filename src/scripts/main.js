39eee559b8737b3bf775040bb4166feda1449dcd
Antoine Pizon <pizon.antoine@gmail.com>
2018-12-03 09:47:24 +0100

    cleaned folder

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,10 @@
 const ws = new WebSlides({
     autoslide: false,
     changeOnClick: false,
-    loop: true,
+    loop: false,
     minWheelDelta: 40,
     navigateOnScroll: true,
     scrollWait: 450,
     slideOffset: 50,
-    showIndex: true
+    showIndex: false
   });
